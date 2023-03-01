8b1954aecc5f4338e43192f73df1540179d52a9a
Lelouch <99410452+Impure-King@users.noreply.github.com>
2023-02-28 22:24:10 -0700

    completed Chapter 1

--- Chapter 1/app.py
+++ Chapter 1/app.py
@@ -1,11 +1,34 @@
-from flask import Flask
+from flask import Flask, request, make_response, redirect, abort
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "<h1> Hello World! </h1>"
+    user_agent = request.headers.get("User-Agent")
+    return f"<p> Your browser is {user_agent[:7]}. </p>"
+
+def lol():
+    return "This is the troll page."
+
+# Adding a url:
+app.add_url_rule('/lol', 'troll', lol)
+
+# Adding a dynamic username section:
+@app.route("/<name>")
+def user(name):
+    return f"<h1> Hello {name}! <h1>"
+
+print(app.url_map)
+
+@app.route("/restricted")
+def restricted():
+    response = make_response("<h1> The document is restricted </h1>")
+    response.set_cookie('answer', '42')
+    return response
+
+if __name__ == "__main__":
+    # The debug=True allows us to reload and see errors.
+    app.run(debug=True)
 
 
-while __name__ == "__main__":
-    app.run()
+# There are some more like redirects and aborts, which just change or interrupt the webpage, if a criteria isn't met.
